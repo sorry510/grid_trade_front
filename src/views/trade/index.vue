2dc6b2218c1351fa300f268e71ad459d1fd5bf6d
sorry510 <491559675@qq.com>
2021-10-08 23:11:41 +0800

    feat: 更新ui和新功能

--- src/views/trade/index.vue
+++ src/views/trade/index.vue
@@ -9,11 +9,12 @@
       size="mini"
       highlight-current-row
     >
-      <el-table-column align="center" label="ID" width="65">
+      <!-- <el-table-column align="center" label="ID" width="65">
         <template slot-scope="scope">
           {{ scope.$index + 1 }}
         </template>
-      </el-table-column>
+      </el-table-column> -->
+
       <el-table-column
         label="币种"
         align="center"
@@ -22,6 +23,92 @@
       >
         <template slot-scope="scope">
           {{ scope.row.symbol }}
+        </template>
+      </el-table-column>
+      <el-table-column type="expand" label=">>" align="center" width="40">
+        <template slot-scope="{ row }">
+          <el-table
+            :data="row.history_trade"
+            border
+            fit
+            size="mini"
+            highlight-current-row
+          >
+            <el-table-column align="center" label="ID" width="65">
+              <template slot-scope="scope">
+                {{ scope.$index + 1 }}
+              </template>
+            </el-table-column>
+            <!-- <el-table-column
+              label="币种"
+              align="center"
+              width="100"
+              show-overflow-tooltip
+            >
+              <template slot-scope="{ row }">
+                {{ row.symbol }}
+              </template>
+            </el-table-column> -->
+            <el-table-column
+              label="交易价格"
+              align="center"
+              show-overflow-tooltip
+            >
+              <template slot-scope="{ row }">
+                {{ row.price }}
+              </template>
+            </el-table-column>
+            <el-table-column
+              label="交易数量"
+              align="center"
+              width="80"
+              show-overflow-tooltip
+            >
+              <template slot-scope="{ row }">
+                {{ row.quantity }}
+              </template>
+            </el-table-column>
+            <el-table-column label="交易方向" align="center" width="75">
+              <template slot-scope="{ row }">
+                <span v-if="row.side === 'BUY'" style="color: green">买入</span>
+                <span v-else style="color: red">卖出</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="已卖出" align="center" width="65">
+              <template slot-scope="{ row }">
+                <span v-if="row.isSell === true" style="color: green">是</span>
+                <span v-else-if="row.isSell === false" style="color: red"
+                  >否
+                </span>
+                <span v-else>-</span>
+              </template></el-table-column
+            >
+            <el-table-column label="收益" align="center" show-overflow-tooltip>
+              <template slot-scope="{ row }">
+                {{ row.profit ? round(row.profit || 0) : '-' }}
+              </template></el-table-column
+            >
+            <el-table-column
+              label="预计卖价"
+              align="center"
+              show-overflow-tooltip
+            >
+              <template slot-scope="{ row }">
+                {{ row.sell_price || '-' }}
+              </template>
+            </el-table-column>
+
+            <el-table-column label="止损率" align="center" width="65">
+              <template slot-scope="{ row }">
+                {{ row.side === 'BUY' ? `${row.stop_loss || 0}%` : '-' }}
+              </template></el-table-column
+            >
+            <el-table-column label="交易时间" align="center" width="140">
+              <template slot-scope="{ row }">
+                {{ row.time }}
+              </template>
+            </el-table-column>
+          </el-table>
         </template>
       </el-table-column>
       <el-table-column
@@ -35,7 +122,7 @@
         </template>
       </el-table-column>
       <el-table-column
-        label="止盈率"
+        label="收益率"
         align="center"
         width="70"
         show-overflow-tooltip
@@ -77,99 +164,24 @@
           {{ scope.row.stop_loss || '-' }}
         </template>
       </el-table-column>
-      <el-table-column label="开启" align="center" width="80">
+      <el-table-column label="买单开启" align="center" width="80">
         <template slot-scope="{ row }">
           <el-switch
-            v-model="row.open"
+            v-model="row.buy_open"
+            active-color="#13ce66"
+            inactive-color="#dcdfe6"
+            @change="isStop($event, row, 'buy_price')"
+          /> </template
+      ></el-table-column>
+      <el-table-column label="卖单开启" align="center" width="80">
+        <template slot-scope="{ row }">
+          <el-switch
+            v-model="row.sell_open"
             active-color="#13ce66"
             inactive-color="#dcdfe6"
             @change="isStop($event, row)"
           /> </template
       ></el-table-column>
-      <el-table-column type="expand" label="交易记录" align="center" width="80">
-        <template slot-scope="{ row }">
-          <el-table
-            :data="row.history_trade"
-            border
-            fit
-            size="mini"
-            highlight-current-row
-          >
-            <el-table-column align="center" label="ID" width="65">
-              <template slot-scope="scope">
-                {{ scope.$index + 1 }}
-              </template>
-            </el-table-column>
-            <!-- <el-table-column
-              label="币种"
-              align="center"
-              width="100"
-              show-overflow-tooltip
-            >
-              <template slot-scope="{ row }">
-                {{ row.symbol }}
-              </template>
-            </el-table-column> -->
-            <el-table-column
-              label="交易数量"
-              align="center"
-              width="80"
-              show-overflow-tooltip
-            >
-              <template slot-scope="{ row }">
-                {{ row.quantity }}
-              </template>
-            </el-table-column>
-            <el-table-column
-              label="交易价格"
-              align="center"
-              show-overflow-tooltip
-            >
-              <template slot-scope="{ row }">
-                {{ row.price }}
-              </template>
-            </el-table-column>
-            <el-table-column label="交易方向" align="center" width="75">
-              <template slot-scope="{ row }">
-                {{ row.side === 'BUY' ? '买入' : '卖出' }}
-              </template>
-            </el-table-column>
-            <el-table-column
-              label="预计卖价"
-              align="center"
-              show-overflow-tooltip
-            >
-              <template slot-scope="{ row }">
-                {{ row.sell_price || '-' }}
-              </template>
-            </el-table-column>
-            <el-table-column label="收益" align="center" show-overflow-tooltip>
-              <template slot-scope="{ row }">
-                {{ row.profit ? round(row.profit || 0) : '-' }}
-              </template></el-table-column
-            >
-            <el-table-column label="已卖出" align="center" width="65">
-              <template slot-scope="{ row }">
-                <span v-if="row.isSell === true" style="color: green">是</span>
-                <span v-else-if="row.isSell === false" style="color: red"
-                  >否
-                </span>
-                <span v-else>-</span>
-              </template></el-table-column
-            >
-            <el-table-column label="止损率" align="center" width="65">
-              <template slot-scope="{ row }">
-                {{ row.side === 'BUY' ? `${row.stop_loss || 0}%` : '-' }}
-              </template></el-table-column
-            >
-            <el-table-column label="交易时间" align="center" width="140">
-              <template slot-scope="{ row }">
-                {{ row.time }}
-              </template>
-            </el-table-column>
-          </el-table>
-        </template>
-      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -188,7 +200,7 @@ export default {
   },
   created() {
     this.fetchData()
-    this.timeId = setInterval(() => this.fetchData(), 30 * 1000)
+    this.timeId = setInterval(() => this.fetchData(), 120 * 1000)
   },
   methods: {
     async fetchData() {
@@ -220,7 +232,14 @@ export default {
     round(data) {
       return round(data)
     },
-    async isStop(event, row) {
+    async isStop(event, row, field = null) {
+      if (event === true && field) {
+        // 重新开启的时候，买的价格定为0，重新开始生成
+        const find = this.list.find((item) => item.symbol === row.symbol)
+        if (find) {
+          find[field] = 0
+        }
+      }
       this.listLoading = true
       try {
         await setTrades({
